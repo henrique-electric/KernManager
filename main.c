9ebde80f5243e5ec379b81a940c8d8cd4187d75b
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-01-24 19:14:10 -0300

    Update main.c
    Finish the parsing of kernel version

--- main.c
+++ main.c
@@ -1,35 +1,62 @@
 #include "core.h"
+#include <regex.h>
+#include <assert.h>
 
 struct kernel_version parse_version(char *version) {
-    size_t str_lenght = strlen(version);
     struct kernel_version kversion = {0};
 
-    for (size_t i = 0; i < str_lenght; i++) {
-        // Major number
-        if (i == 0 && version[i + 1] == '.')
-            kversion.major = (uint8_t) version[i] - '0';
+// Stuff used by regex.h functions
+    regex_t compiled_regex;
+    regmatch_t match[4];
+    const char *pattern = "^([0-9])\\.([0-9]{1,2})\\.([0-9]{1,3})$"; // Regex pattern used on the kernel version
+//
 
-        // Middel for model X.XX.X
-        if (i == 1 && version[i + 3] == '.') {
-            kversion.numbers_middle = 2;
-            kversion.middle = (((int) version[i + 1] - '0') * 10) + ((int) version[i + 2] - '0');
-        }
-        // Middle for model X.X.X
-        else if (i == 1 && version[i + 2] == '.') {
-            kversion.numbers_middle = 1;
-            kversion.middle = (int) version[i + 1] - '0';
-        }
+//  Compile the regex
+    int compile_status = regcomp(&compiled_regex, pattern, REG_EXTENDED);
+    assert(compile_status == 0);
+//
 
-        // Minor version for model X.X.X
-        if (i == 3 && kversion.numbers_middle == 1 && version[i + 2] == '\0'){
-            kversion.minor = (int) version[i + 1] - '0';
-        }
-        // Model
-        else if (i == 3) {
+    const char *str = version; // create a new pointer that points to the version string, since we'll do operations with offsets
 
-        }
-
+// Search for the patterns on the version string and validate if the version was written correct
+    int exec_status = regexec(&compiled_regex, str, 4, match, 0);
+    if (exec_status == REG_NOMATCH) {
+        fprintf(stderr, "Invalid version of the kernel\n");
+        exit(EXIT_FAILURE);
     }
+//
+
+    kversion.major = (int) str[match[1].rm_so] - '0';
+
+    int converted_number = 0;   // Variable that will be used when converting the strings to integers;
+
+    // Run throught the matches
+    for (size_t i=2; i < 4; i++) {
+        size_t offset = match[i].rm_eo - match[i].rm_so; // Offset between the first digit to the last digit of the string match
+
+        // If the offset is two, we copy the two digits into a buffer with and then convert to an integer
+        if (offset == 2) {
+            char buff[3] = {0};
+            snprintf(buff, 3,"%.*s", offset, str + match[i].rm_so);
+            converted_number = atoi(buff);
+
+            // Since both the middle and minor number can have two digits we'll handle both cases
+            if (i == 2)
+                kversion.middle = converted_number;
+            else
+                kversion.minor = converted_number;
+        }
+
+        if (offset == 3) {
+            char buff[4] = {0};
+            snprintf(buff, 4, "%.*s", offset, str + match[i].rm_so);
+            converted_number = atoi(buff);
+            kversion.minor = converted_number;
+        }
+    }
+
+    
+    regfree(&compiled_regex); // Free the regex allocated stuff
     return kversion;
 }
 
