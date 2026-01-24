cff545283b4383cdcdaadea2981c3b8c45098804
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-01-24 10:33:52 -0300

    Finish arg parse structure

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <argp.h>
 #include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct argp_option options[] = {
     {.key = 'd', .name = "download", .arg = "VERSION", .doc = "Downloads a new kernel source from kernel.org"},
@@ -7,8 +9,32 @@ struct argp_option options[] = {
     {NULL},
 };
 
-int main(int argc, char const *argv[])
-{
+error_t parser(int key, char *arg, struct argp_state *state) {
+
+    if (state->argc == 1) {
+        argp_help(state->root_argp, stdout, ARGP_HELP_STD_HELP, "KernManager");
+        exit(EXIT_FAILURE);
+    }
     
+    switch (key)
+    {
+    case 'd':
+        ;
+        break;
+    
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+
     return 0;
+}
+
+struct argp argp_struct = {
+    .options = options,
+    .parser = parser
+};
+
+int main(int argc, char *argv[])
+{
+    return argp_parse(&argp_struct, argc, argv, 0, 0, 0);;
 }
