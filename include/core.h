95aef4b6080969e0671368bb6e0c3ec3711b3214
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-01-30 20:25:02 -0300

    Update core.h
    Add functions to handle the config files for building the kernel

--- include/core.h
+++ include/core.h
@@ -5,6 +5,7 @@
 #include <argp.h>
 #include <string.h>
 #include <stdint.h>
+#include <sys/stat.h>
 
 #define ERROR_HANDLE_LINK -1
 #define SUCCESS_HANDLE_LINK 0
@@ -12,7 +13,9 @@
 #define ERROR_DOWNLOAD_KERNEL -1
 #define SUCCESS_DOWNLOAD_KERNEL 0
 
-#define MINOR_VERSION_NOT_SPECIFIED -1  // For when the 
+#define MINOR_VERSION_NOT_SPECIFIED -1  // For when the
+
+#define CONFIG_FILE_DOESNT_EXIST -1
 
 struct kernel_version {
     int major;
@@ -26,9 +29,13 @@ error_t parse_args(int argc, char *argv[]);
 int handle_link(const struct kernel_version *kversion);
 int download_kernel(char *link);
 int unzip_kernel(const struct kernel_version *version);
+int query_config_file(const struct kernel_version *kver);
+void create_config_dir(void);
+const char *get_home_dir(void);
 
 extern struct argp_option options[];
 extern struct argp argp_struct;
 extern const char *kernel_url;
+extern const char *config_dir;
 
 #endif
